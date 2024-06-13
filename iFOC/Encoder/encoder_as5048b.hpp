e8bf364b4d105791587499da71b6f33aabc29f61
Matr1x. <vip99013zhq@outlook.com>
2024-06-13 23:17:24 +0800

    24/6/13: Added AS5048B Encoder, and changed AS5600

---
+++ iFOC/Encoder/encoder_as5048b.hpp
@@ -0,0 +1,75 @@
+#ifndef _FOC_ENCODER_AS5048B_BASE_H
+#define _FOC_ENCODER_AS5048B_BASE_H
+
+#include "encoder_base.h"
+#include "i2c_base.h"
+
+template <typename T = I2CBase>
+class EncoderAS5048B : public EncoderBase
+{
+public:
+    EncoderAS5048B(T& _i2c, uint8_t _addr) : i2c(_i2c), device_addr(_addr)
+    {
+        static_assert(std::is_base_of<I2CBase, T>::value, "I2C Implementation must be derived from I2CBase");
+    };
+    bool Init(float max_vel) override;
+    void Update(float Ts) override;
+    void UpdateMidInterval(float Ts) override;
+    bool IsCalibrated() override;
+private:
+    T& i2c;
+    uint8_t device_addr = 0x00;
+    uint8_t i2c_buffer[2] = {0};
+    uint16_t raw_14bit_angle = 0;
+    float single_round_angle_prev = -1.0f;
+    SlidingFilter sliding_filter = SlidingFilter(10);
+    LowpassFilter speed_filter = LowpassFilter(0.001f);
+    float last_raw_angle = 0.0f;
+    // uint8_t repeat_counter = 0; // runs at 2KHz
+};
+
+template <typename T>
+bool EncoderAS5048B<T>::Init(float max_vel)
+{
+    max_velocity = max_vel;
+    i2c.Init();
+    return true;
+}
+
+template <typename T>
+void EncoderAS5048B<T>::Update(float Ts)
+{
+    ; // update interval may be too short for I2C communication
+}
+
+template <typename T>
+void EncoderAS5048B<T>::UpdateMidInterval(float Ts)
+{
+    uint8_t temp = 0xFE;
+    i2c.WriteBytes(device_addr, &temp, 1);
+    i2c.ReadBytes(device_addr, i2c_buffer, 2);
+    raw_14bit_angle = ((uint16_t)i2c_buffer[0] << 6) | (i2c_buffer[1] & 0x1F);
+    single_round_angle = normalize_rad((float)raw_14bit_angle / 16383.0f * PI2);
+    if(single_round_angle_prev >= 0.0f)
+    {
+        float delta = single_round_angle - single_round_angle_prev;
+        if(delta > 0.8f * PI2) full_rotations -= 1;
+        else if(delta < -0.8f * PI2) full_rotations += 1;
+        raw_angle = full_rotations * PI2 + single_round_angle;
+        float vel = (float)(raw_angle - last_raw_angle) / Ts;
+        // vel = sliding_filter.GetOutput(vel);
+        vel = speed_filter.GetOutput(vel, Ts);
+        last_raw_angle = raw_angle;
+        velocity = sliding_filter.GetOutput(vel);
+    }
+    single_round_angle_prev = single_round_angle;
+    // repeat_counter = 0;
+}
+
+template <typename T>
+bool EncoderAS5048B<T>::IsCalibrated()
+{
+    return true;
+}
+
+#endif
