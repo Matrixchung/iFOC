2c52c101eb2dc9063199401af68183560cd9e975
Matr1x. <vip99013zhq@outlook.com>
2024-07-17 02:43:36 +0800

    24/7/17: Also fixed normal PID class

--- iFOC/Common/pid.cpp
+++ iFOC/Common/pid.cpp
@@ -3,14 +3,14 @@ float PID::GetOutput(float error, float Ts)
 {
     float integral = 0.0f;
     float output = Kp * error;
-    if(enable_integral)
+    output = _constrain(output, -limit, limit);
+    if(Ki != 0.0f && enable_integral)
     {
         integral = _constrain(integral_prev + (Ki * Ts * 0.5f * (error + error_prev)), -limit - output, limit - output);
         integral_prev = integral;
     }
     else integral_prev = 0.0f;
     output += integral + (Kd * (error - error_prev) / Ts);
-    // float output = (Kp * error) + integral + (Kd * (error - error_prev) / Ts);
     output = _constrain(output, -limit, limit);
     if(ramp_limit > 0.0f)
     {
