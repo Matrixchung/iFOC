bcb8eb357df3a9d4aa5735ac729b01d69038e014
Matr1x. <vip99013zhq@outlook.com>
2024-11-10 22:59:01 +0800

    24/11/10: Sync UARTProtocol with ECAT interface

--- iFOC/Protocols/uart_protocol.hpp
+++ iFOC/Protocols/uart_protocol.hpp
@@ -4,125 +4,241 @@
 #include <cstdint>
 #include "utils.h"
 #include "string.h"
+#include "sys/param.h"
 
-#define KEEPALIVE_TICKS 500
-
-typedef struct motor_state_t
+class motor_state_t
 {
-    uint8_t IsAlive;
-    uint8_t Enable;
-    uint8_t Mode;
-    uint8_t MotorTemp;
-    uint8_t MCUTemp;
-    uint8_t LimiterState;
-    uint16_t ErrorCode;
-    uint32_t SN;
-    float IqReal;
-    float IdReal;
-    float IqSet;
-    float IdSet;
-    float Vbus;
-    float Ibus;
-    float EstimateSpeed;
-    float EstimateRawAngle;
-    float CurrentLimit;
-}motor_state_t;
+public:
+    uint8_t IsAlive = 0;
+    uint8_t Enable = 0;
+    uint8_t NodeID = 0;
+    uint8_t Mode = 0;
+    uint8_t MotorTemp = 0;
+    uint8_t MCUTemp = 0;
+    uint8_t LimiterState = 0;
+    uint16_t ErrorCode = 0;
+    uint32_t SN = 0;
+    float IqReal = 0.0f;
+    float IdReal = 0.0f;
+    float Vbus = 0.0f;
+    float Ibus = 0.0f;
+    float EstimateSpeed = 0.0f;
+    float EstimateRawAngle = 0.0f;
+    float CurrentLimit = 0.0f;
+    bool operator==(const motor_state_t& rhs) const { return memcmp(&IsAlive, &rhs.IsAlive, sizeof(motor_state_t)) == 0; }
+    bool operator!=(const motor_state_t& rhs) const { return !(*this == rhs); }
+};
 
-typedef struct motor_set_t
+class motor_set_t
 {
-    uint8_t Enable;
-    uint8_t Identify;
-    uint8_t Mode;
-    float IqSet;
-    float IdSet;
-    float SetSpeed;
-    float SetRawAngle;
-    float SetTrajectoryAngle;
-    float SetCurrentLimit;
-}motor_set_t;
+public:
+    uint8_t Enable = 0;
+    uint8_t Mode = 0;
+    float IqSet = 0.0f;
+    float SetSpeed = 0.0f;
+    float SetRawAngle = 0.0f;
+    float SetTrajectoryAngle = 0.0f;
+    float SetCurrentLimit = 0.0f;
+    bool operator==(const motor_set_t& rhs) const { return memcmp(&Enable, &rhs.Enable, sizeof(motor_set_t)) == 0; }
+    bool operator!=(const motor_set_t& rhs) const { return !(*this == rhs); }
+};
+
+#define PDO_PAYLOAD_LEN 42
+#define MOTOR_STATE_HEADER 0x43
+#define PAYLOAD_HEADER 0x57
+#define UART_TIMEOUT_TICKS 500
+#define UART_KEEPALIVE_TICKS 100
+
+class state_buffer_t // upstream to Interface, SN packed in payload
+{
+public:
+    uint8_t header = 0;
+    uint8_t crc16[2] = {0};
+    uint8_t __attribute__((aligned(4))) payload[MAX(sizeof(motor_state_t), PDO_PAYLOAD_LEN)] = {0};
+    bool operator==(const state_buffer_t& rhs) const { return memcmp(&header, &rhs.header, sizeof(state_buffer_t)) == 0; }
+    bool operator!=(const state_buffer_t& rhs) const { return !(*this == rhs); }
+    bool isValid()
+    {
+        if(header != MOTOR_STATE_HEADER && header != PAYLOAD_HEADER) return false;
+        uint16_t crc_16 = 0;
+        memcpy(&crc_16, crc16, 2);
+        if(crc_16 == getCRC16(payload, sizeof(payload))) return true;
+        return false;
+    }
+    void prepare()
+    {
+        uint16_t crc_16 = getCRC16(payload, sizeof(payload));
+        memcpy(crc16, &crc_16, 2);
+    }
+    [[nodiscard]] motor_state_t * state() const { return (motor_state_t*)payload; }
+};
+
+class set_buffer_t  // Interface downstream to Driver, need SN
+{
+public:
+    uint8_t header = 0;
+    uint8_t crc16[2] = {0};
+    uint8_t SN[4] = {0};
+    uint8_t __attribute__((aligned(4))) payload[MAX(sizeof(motor_set_t), PDO_PAYLOAD_LEN)] = {0};
+    bool operator==(const set_buffer_t& rhs) const { return memcmp(&header, &rhs.header, sizeof(set_buffer_t)) == 0; }
+    bool operator!=(const set_buffer_t& rhs) const { return !(*this == rhs); }
+    bool isValid()
+    {
+        if(header != MOTOR_STATE_HEADER && header != PAYLOAD_HEADER) return false;
+        uint16_t crc_16 = 0;
+        memcpy(&crc_16, crc16, 2);
+        if(crc_16 == getCRC16(SN, sizeof(payload) + sizeof(SN))) return true;
+        return false;
+    }
+    void prepare()
+    {
+        uint16_t crc_16 = getCRC16(SN, sizeof(payload) + sizeof(SN));
+        memcpy(crc16, &crc_16, 2);
+    }
+    void setSN(uint32_t _SN) { memcpy(SN, &_SN, 4); }
+    uint32_t getSN()
+    {
+        uint32_t ret = 0;
+        memcpy(&ret, SN, 4);
+        return ret;
+    }
+    [[nodiscard]] motor_set_t* set() const { return (motor_set_t*)payload; }
+};
+
+typedef void (*rx_payload_cb)(uint8_t *payload, uint8_t len);
 
 class UARTProtocol
 {
-// private:
 public:
-    const uint8_t _head[2] = {0x11, 0x45};
-public:
-    UARTProtocol();
-    motor_state_t state;
-    motor_set_t set;
-    uint8_t state_buffer[sizeof(_head) + sizeof(motor_state_t) + sizeof(uint16_t)];
-    uint8_t set_buffer[sizeof(_head) + sizeof(state.SN) + sizeof(motor_set_t) + sizeof(uint16_t)]; // added SN identifier
-    uint32_t last_rec_tick = 0;
-    bool keep_alive = false;
-    bool IsAlive();
-    void PrepareTxStateBuf();
-    void PrepareTxSetBuf();
-    bool ReceiveRxStateBuf(uint8_t *buffer, uint8_t len);
-    bool ReceiveRxSetBuf(uint8_t *buffer, uint8_t len);
+    state_buffer_t state_buffer;
+    set_buffer_t set_buffer;
+    rx_payload_cb _payload_cb = nullptr;
+    bool requestedTx = false;
+    void PrepareStateBuf(motor_state_t *state);
+    void PrepareSetBuf(motor_set_t *set);
+    void PreparePayloadToStateBuf(char* payload, uint8_t len, uint32_t SN);
+    void PreparePayloadToSetBuf(char* payload, uint8_t len);
+    bool ReceiveStateBuf(char* buffer, uint16_t max_len);
+    bool ReceiveSetBuf(char* buffer, uint16_t max_len, uint32_t SN);
+    [[nodiscard]] bool IsAlive() const;
+    uint32_t _SN = 0; // for master, will be updated in ReceiveStateBuf()
+private:
+    uint32_t last_tx_tick = 0;
+    uint32_t last_rx_tick = 0;
 };
 
-bool UARTProtocol::IsAlive()
+bool UARTProtocol::ReceiveStateBuf(char* buffer, uint16_t max_len)
 {
-    if(HAL_GetTick() - last_rec_tick >= KEEPALIVE_TICKS) keep_alive = false;
-    else keep_alive = true;
-    return keep_alive;
-}
-
-void UARTProtocol::PrepareTxStateBuf()
-{
-    memcpy(state_buffer, _head, sizeof(_head));
-    memcpy(state_buffer + sizeof(_head), &state, sizeof(motor_state_t));
-    uint16_t crc_16 = getCRC16(state_buffer, sizeof(_head) + sizeof(motor_state_t));
-    state_buffer[sizeof(_head) + sizeof(motor_state_t)] = (uint8_t)(crc_16 >> 8);
-    state_buffer[sizeof(_head) + sizeof(motor_state_t) + 1] = (uint8_t)(crc_16);
-}
-
-void UARTProtocol::PrepareTxSetBuf()
-{
-    memcpy(set_buffer, _head, sizeof(_head));
-    // memcpy(set_buffer + sizeof(_head), &set, sizeof(motor_set_t));
-    memcpy(set_buffer + sizeof(_head), &state.SN, sizeof(state.SN));
-    memcpy(set_buffer + sizeof(_head) + sizeof(state.SN), &set, sizeof(motor_set_t));
-    uint16_t crc_16 = getCRC16(set_buffer, sizeof(_head) + sizeof(state.SN) + sizeof(motor_set_t));
-    set_buffer[sizeof(_head) + sizeof(state.SN) + sizeof(motor_set_t)] = (uint8_t)(crc_16 >> 8);
-    set_buffer[sizeof(_head) + sizeof(state.SN) + sizeof(motor_set_t) + 1] = (uint8_t)(crc_16);
-}
-
-bool UARTProtocol::ReceiveRxStateBuf(uint8_t *buffer, uint8_t len)
-{
-    if(len != sizeof(state_buffer)) return false;
-    for(uint8_t i = 0; i < sizeof(_head); i++)
+    uint16_t ret = 0;
+    while(ret < max_len)
     {
-        if(buffer[i] != _head[i]) return false;
+        if(*(buffer + ret) == MOTOR_STATE_HEADER || *(buffer + ret) == PAYLOAD_HEADER) break;
+        ret++;
     }
-    uint16_t crc = (uint16_t)buffer[sizeof(_head) + sizeof(motor_state_t) + 1] | (uint16_t)(buffer[sizeof(_head) + sizeof(motor_state_t)] << 8);
-    if(crc != getCRC16(buffer, sizeof(_head) + sizeof(motor_state_t))) return false;
-    memcpy(&state, buffer + sizeof(_head), sizeof(motor_state_t));
-    last_rec_tick = HAL_GetTick();
-    return true;
-}
-
-bool UARTProtocol::ReceiveRxSetBuf(uint8_t *buffer, uint8_t len)
-{
-    if(len != sizeof(set_buffer)) return false;
-    for(uint8_t i = 0; i < sizeof(_head); i++)
+    // if(ret + sizeof(state_buffer) > max_len) return false;
+    auto *rx_state = (state_buffer_t*)(buffer + ret);
+    if((*rx_state).isValid())
     {
-        if(buffer[i] != _head[i]) return false;
+        memcpy(&state_buffer, buffer + ret, sizeof(state_buffer));
+        if((*rx_state).header == PAYLOAD_HEADER)
+        {
+            // memcpy(&_SN, (*rx_state).payload, sizeof(_SN));
+            if(_payload_cb) _payload_cb(state_buffer.payload + 4, sizeof(state_buffer.payload) - 4);
+        }
+        else _SN = (*rx_state).state()->SN;
+        last_rx_tick = HAL_GetTick();
+        return true;
     }
-    uint16_t crc = (uint16_t)buffer[sizeof(_head) + sizeof(state.SN) + sizeof(motor_set_t) + 1] | (uint16_t)(buffer[sizeof(_head) + sizeof(state.SN) + sizeof(motor_set_t)] << 8);
-    if(crc != getCRC16(buffer, sizeof(_head) + sizeof(state.SN) + sizeof(motor_set_t))) return false;
-    uint32_t SN = 0;
-    memcpy(&SN, buffer + sizeof(_head), sizeof(SN));
-    if(SN != state.SN) return false;
-    memcpy(&set, buffer + sizeof(_head) + sizeof(state.SN), sizeof(motor_set_t));
-    last_rec_tick = HAL_GetTick();
-    return true;
+    return false;
 }
 
-UARTProtocol::UARTProtocol()
+bool UARTProtocol::ReceiveSetBuf(char* buffer, uint16_t max_len, uint32_t SN)
 {
-    memset(&state, 0, sizeof(state));
-    memset(&set, 0, sizeof(set));
+    if(max_len < sizeof(set_buffer)) return false;
+    uint16_t ret = 0;
+    auto *rx_set = (set_buffer_t*)(buffer + ret);
+    while(ret < max_len)
+    {
+        rx_set = (set_buffer_t*)(buffer + ret);
+        if(((*rx_set).header == MOTOR_STATE_HEADER || (*rx_set).header == PAYLOAD_HEADER) &&
+           (*rx_set).getSN() == SN) break;
+        ret++;
+    }
+    if(ret + sizeof(set_buffer) > max_len) return false;
+    if((*rx_set).isValid())
+    {
+        memcpy(&set_buffer, rx_set, sizeof(set_buffer));
+        if((*rx_set).header == PAYLOAD_HEADER)
+        {
+            if(_payload_cb) _payload_cb(set_buffer.payload, sizeof(set_buffer.payload));
+        }
+        last_rx_tick = HAL_GetTick();
+        return true;
+    }
+    return false;
+}
+
+void UARTProtocol::PrepareStateBuf(motor_state_t *state)
+{
+    if(requestedTx) return;
+    if(HAL_GetTick() - last_tx_tick < UART_KEEPALIVE_TICKS) // within keepalive period, we can choose to skip
+    {
+        if(*state == *(state_buffer.state())) return;
+    }
+    memcpy(state_buffer.payload, state, sizeof(motor_state_t));
+    state_buffer.header = MOTOR_STATE_HEADER;
+    state_buffer.prepare();
+    last_tx_tick = HAL_GetTick();
+    requestedTx = true;
+}
+
+void UARTProtocol::PrepareSetBuf(motor_set_t *set)
+{
+    if(requestedTx) return;
+    if(_SN == 0) return;
+    if(HAL_GetTick() - last_tx_tick < UART_KEEPALIVE_TICKS) // within keepalive period, we can choose to skip
+    {
+        if(*set == *(set_buffer.set())) return;
+    }
+    memcpy(set_buffer.payload, set, sizeof(motor_set_t));
+    set_buffer.header = MOTOR_STATE_HEADER;
+    set_buffer.setSN(_SN);
+    set_buffer.prepare();
+    last_tx_tick = HAL_GetTick();
+    requestedTx = true;
+}
+
+void UARTProtocol::PreparePayloadToStateBuf(char* payload, uint8_t len, uint32_t SN) // payload uniqueness is guaranteed by upstream protocol
+{
+    // if(requestedTx) return;
+    if(len > sizeof(state_buffer.payload) - 4) len = sizeof(state_buffer.payload) - 4;
+    memset(state_buffer.payload, 0, sizeof(state_buffer.payload));
+    memcpy(state_buffer.payload, &SN, sizeof(SN));
+    memcpy(state_buffer.payload + 4, payload, len);
+    state_buffer.header = PAYLOAD_HEADER;
+    state_buffer.prepare();
+    last_tx_tick = HAL_GetTick();
+    requestedTx = true;
+}
+
+void UARTProtocol::PreparePayloadToSetBuf(char* payload, uint8_t len)
+{
+    // if(requestedTx) return;
+    if(_SN == 0) return;
+    if(len > sizeof(set_buffer.payload)) len = sizeof(set_buffer.payload);
+    memset(set_buffer.payload, 0, sizeof(set_buffer.payload));
+    memcpy(set_buffer.payload, payload, len);
+    set_buffer.header = PAYLOAD_HEADER;
+    set_buffer.setSN(_SN);
+    set_buffer.prepare();
+    last_tx_tick = HAL_GetTick();
+    requestedTx = true;
+}
+
+bool UARTProtocol::IsAlive() const
+{
+    if(HAL_GetTick() - last_rx_tick < UART_TIMEOUT_TICKS) return true;
+    return false;
 }
 
 #endif
