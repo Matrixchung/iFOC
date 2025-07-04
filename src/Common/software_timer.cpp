1ebdf6cac5b9bd36c8493d2e8e41c90b147f3436
Matrixchung <xin-zhong@163.com>
2025-07-04 18:44:08 +0800

    Fixed Software Timer's bugs
    and expand the id range to 0-65535 (uint16_t)

--- src/Common/software_timer.cpp
+++ src/Common/software_timer.cpp
@@ -1,8 +1,23 @@
 #include "software_timer.hpp"
 
+#include <algorithm>
+
 namespace iFOC
 {
-FuncRetCode SoftwareTimer::AddTimer(uint8_t id, uint32_t interval, TimerCallback cb)
+uint16_t SoftwareTimer::GetUnusedTimerID()
+{
+    for(uint16_t id = 0; id < 0xFFFF; id++)
+    {
+        const auto it = std::find_if(timers.cbegin(), timers.cend(), [id](const Timer& t)
+        {
+           return t.id == id;
+        });
+        if(it == timers.cend()) return id;
+    }
+    return 0xFFFF;
+}
+
+FuncRetCode SoftwareTimer::AddTimer(uint16_t id, uint32_t interval, TimerCallback cb)
 {
     if(!cb || interval == 0) return FuncRetCode::INVALID_INPUT;
     for(const auto& timer : timers)
@@ -13,20 +28,24 @@ FuncRetCode SoftwareTimer::AddTimer(uint8_t id, uint32_t interval, TimerCallback
     return FuncRetCode::OK;
 }
 
-FuncRetCode SoftwareTimer::DelTimer(uint8_t id)
+FuncRetCode SoftwareTimer::DelTimer(uint16_t id)
 {
-    const auto removed = timers.remove_if([id](const Timer& timer)
-    {
-        if(timer.id == id) return true;
-        return false;
-    });
-    if(removed > 0) return FuncRetCode::OK;
-    return FuncRetCode::PARAM_NOT_EXIST;
+    // const auto removed = timers.remove_if([id](const Timer& timer)
+    // {
+    //     if(timer.id == id || timer.set_interval == 0) return true;
+    //     return false;
+    // });
+    // if(removed > 0) return FuncRetCode::OK;
+    // return FuncRetCode::PARAM_NOT_EXIST;
+
+    // Delete timer by set the interval to 0, instead of directly delete it
+    // To prevent call DelTimer(id) inside timer.callback()
+    return SetTimerInterval(id, 0);
 }
 
-FuncRetCode SoftwareTimer::SetTimerInterval(uint8_t id, uint32_t interval)
+FuncRetCode SoftwareTimer::SetTimerInterval(uint16_t id, uint32_t interval)
 {
-    if(interval == 0) return FuncRetCode::INVALID_INPUT;
+    // if(interval == 0) return FuncRetCode::INVALID_INPUT;
     for(auto& timer : timers)
     {
         if(timer.id == id)
@@ -39,16 +58,36 @@ FuncRetCode SoftwareTimer::SetTimerInterval(uint8_t id, uint32_t interval)
     return FuncRetCode::PARAM_NOT_EXIST;
 }
 
+FuncRetCode SoftwareTimer::ResetTimer(uint16_t id)
+{
+    for(auto& timer : timers)
+    {
+        if(timer.id == id)
+        {
+            timer.curr_val = 0;
+            return FuncRetCode::OK;
+        }
+    }
+    return FuncRetCode::PARAM_NOT_EXIST;
+}
+
 void SoftwareTimer::UpdateTimer(uint32_t tickrate)
 {
     for(auto& timer : timers)
     {
-        timer.curr_val += tickrate;
-        if(timer.curr_val >= timer.set_interval)
+        if(timer.set_interval > 0)
         {
-            if(timer.callback) timer.callback(timer.id);
-            timer.curr_val = 0;
+            timer.curr_val += tickrate;
+            if(timer.curr_val >= timer.set_interval)
+            {
+                if(timer.callback) timer.callback(timer.id); // MUST NOT call remove_if inside callback()
+                timer.curr_val = 0;
+            }
         }
     }
+    timers.remove_if([](const Timer& timer) // clear unused timer
+    {
+        return timer.set_interval == 0;
+    });
 }
 }
--- src/Common/software_timer.hpp
+++ src/Common/software_timer.hpp
@@ -9,19 +9,21 @@ class SoftwareTimer
 public:
     OVERRIDE_NEW();
     DELETE_COPY_CONSTRUCTOR(SoftwareTimer);
-    using TimerCallback = std::function<void(uint8_t id)>;
+    using TimerCallback = std::function<void(uint16_t id)>;
     struct Timer
     {
-        uint8_t id = 0; // UNIQUE id, thus maximum timer count = 256
+        uint16_t id = 0; // UNIQUE id, thus maximum timer count = 65536
         uint32_t curr_val = 0;
         uint32_t set_interval = 0;
         TimerCallback callback = nullptr;
     };
     SoftwareTimer() = default;
     ~SoftwareTimer() = default;
-    FuncRetCode AddTimer(uint8_t id, uint32_t interval, TimerCallback cb);
-    FuncRetCode DelTimer(uint8_t id);
-    FuncRetCode SetTimerInterval(uint8_t id, uint32_t interval);
+    uint16_t GetUnusedTimerID();
+    FuncRetCode AddTimer(uint16_t id, uint32_t interval, TimerCallback cb);
+    FuncRetCode DelTimer(uint16_t id);
+    FuncRetCode SetTimerInterval(uint16_t id, uint32_t interval);
+    FuncRetCode ResetTimer(uint16_t id);
     void UpdateTimer(uint32_t tickrate);
 private:
     List<Timer> timers{};
