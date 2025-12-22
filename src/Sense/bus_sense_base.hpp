81fcb181d1b554a915a0d12e0eb58a6123603bb7
Xin Zhong <xin-zhong@163.com>
2025-12-22 21:16:35 +0800

    Added BusSenseType property to Sense::BusSenseBase

--- src/Sense/bus_sense_base.hpp
+++ src/Sense/bus_sense_base.hpp
@@ -5,14 +5,22 @@
 
 namespace iFOC::Sense
 {
+enum class BusSenseType : uint8_t
+{
+    SINGLE_ENDED = 0,
+    SERIAL = 1
+};
 class BusSenseBase
 {
     OVERRIDE_NEW();
 public:
-    real_t voltage = 0.0f;
-    real_t current = 0.0f;
+    real_t voltage = 0.0f; // [V]
+    real_t current = 0.0f; // [A], positive value means current flow from supply to driver
+    [[nodiscard]] BusSenseType GetBusSenseType() const { return type; }
     virtual FuncRetCode Init() { return FuncRetCode::OK; };
     virtual void Update() = 0;
+protected:
+    BusSenseType type = BusSenseType::SINGLE_ENDED;
 };
 
 template<typename T>
