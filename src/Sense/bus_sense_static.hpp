44c376133ee56ef5b432cee61358e13c0425ab26
Xin Zhong <xin-zhong@163.com>
2025-12-22 21:17:01 +0800

    Added Sense::BusSenseStatic class

---
+++ src/Sense/bus_sense_static.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "bus_sense_base.hpp"
+
+namespace iFOC::Sense
+{
+class BusSenseStatic final : public BusSenseBase
+{
+public:
+    BusSenseStatic() = delete;
+    explicit BusSenseStatic(real_t Vbus);
+    void Update() override {}
+};
+
+inline BusSenseStatic::BusSenseStatic(real_t Vbus)
+{
+    voltage = Vbus;
+    current = 0.0f;
+}
+}
