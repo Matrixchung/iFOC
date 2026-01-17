544db9f654615e6d4354c260c82eb8ea12fe6047
Xin Zhong <xin-zhong@163.com>
2026-01-17 12:14:26 +0800

    Reworked BusSenseINA237

--- src/Sense/bus_sense_ina237.cpp
+++ src/Sense/bus_sense_ina237.cpp
@@ -5,10 +5,9 @@
 namespace iFOC::Sense
 {
 BusSenseINA237::BusSenseINA237(I2CBase *_i2c, uint8_t _addr, float _max, float _shunt, bool rev) :
-        i2c(_i2c), device_addr(_addr), max_current(_max), rshunt_ohm(_shunt), current_lsb(_max / 32768.0f), reversed(rev)
+        i2c(_i2c), device_addr(_addr), max_current(_max), rshunt_ohm(_shunt), reversed(rev)
 {
-    // power_lsb = current_lsb * 0.2f;
-    current_cal_reg = (uint16_t)(819200000.0f * current_lsb * rshunt_ohm);
+    current_lsb = max_current / 32768.0f;
 }
 
 BusSenseINA237::BusSenseINA237(I2CBase *_i2c) :
@@ -30,42 +29,88 @@ FuncRetCode BusSenseINA237::Init()
     {
         return FuncRetCode::PARAM_OUT_BOUND;
     }
-    i2c->Init();
+
+    auto ret = i2c->Init();
+    if(ret != FuncRetCode::OK) return ret;
+
     /// Step #1: Read MANUFACTURER_ID register, it should be 0x5449
-    i2c_buffer[0] = 0x3E;
-    i2c->WriteBytes(device_addr, i2c_buffer, 1);
-    i2c->ReadBytes(device_addr, i2c_buffer, 2);
-    if(i2c_buffer[0] != 0x54 || i2c_buffer[1] != 0x49)
+    uint16_t temp = 0x00;
+    ret = ReadReg(0x3E, &temp);
+    if(ret != FuncRetCode::OK) return ret;
+    if(temp != 0x5449)
     {
-        if(i2c_buffer[0] != 0x00 && i2c_buffer[1] != 0x00) return FuncRetCode::CRC_MISMATCH;
+        if(temp > 0x00) return FuncRetCode::CRC_MISMATCH;
         return FuncRetCode::HARDWARE_ERROR;
     }
+
     /// Step #2: Write ADC_CONFIG register, enabling AVG=1
-    i2c_buffer[0] = 0x01;
-    i2c_buffer[1] = 0xFB;
-    i2c_buffer[2] = 0x69;
-    i2c->WriteBytes(device_addr, i2c_buffer, 3);
-    HAL::DelayMs(100);
+    adc_config adc_cfg{};
+    adc_cfg.bit.avg = 1; // average times = 4
+    adc_cfg.bit.vtct = 6; // temperature conv time = 2074us
+    adc_cfg.bit.vshct = 5; // Vshunt conv time = 1052us
+    adc_cfg.bit.vbusct = 6; // Vbus conv time = 2074us
+    adc_cfg.bit.mode = 0xF; // mode: continuous Vbus, Vshunt & Temp
+    ret = WriteReg(0x01, adc_cfg.reg);
+    if(ret != FuncRetCode::OK) return ret;
+    HAL::DelayMs(10);
+
     /// Step #3: Write Calibration register
-    i2c_buffer[0] = 0x02;
-    i2c_buffer[1] = (uint8_t) (current_cal_reg >> 8);
-    i2c_buffer[2] = (uint8_t) current_cal_reg;
-    i2c->WriteBytes(device_addr, i2c_buffer, 3);
+    /// SHUNT_CAL = 819.2 * 10^6 * current_lsb * rshunt
+    /// CURRENT_LSB = max_current / 2^15
+    shunt_cal s_cal{};
+    s_cal.bit.shunt_cal = (uint16_t)(819200000.0f * current_lsb * rshunt_ohm);
+    ret = WriteReg(0x02, s_cal.reg);
+    if(ret != FuncRetCode::OK) return ret;
+
     return FuncRetCode::OK;
 }
 
-void BusSenseINA237::Update()
+FuncRetCode BusSenseINA237::Update()
 {
-    i2c_buffer[0] = 0x05;
-    i2c->WriteBytes(device_addr, i2c_buffer, 1);
-    i2c->ReadBytes(device_addr, i2c_buffer, 2);
-    uint16_t temp = ((uint16_t) i2c_buffer[0] << 8) | i2c_buffer[1];
+    volatile uint16_t temp = 0;
+    /// Step #1: Read VBUS
+    auto ret = ReadReg(0x05, (uint16_t*)&temp);
+    if(ret != FuncRetCode::OK)
+    {
+        voltage = 0.0f;
+        current = 0.0f;
+        return ret;
+    }
     voltage = (float)temp * 0.003125f;
-    i2c_buffer[0] = 0x07;
-    i2c->WriteBytes(device_addr, i2c_buffer, 1);
-    i2c->ReadBytes(device_addr, i2c_buffer, 2);
-    temp = ((uint16_t) i2c_buffer[0] << 8) | i2c_buffer[1];
+
+    /// Step #2: Read CURRENT
+    ret = ReadReg(0x07, (uint16_t*)&temp);
+    if(ret != FuncRetCode::OK)
+    {
+        voltage = 0.0f;
+        current = 0.0f;
+        return ret;
+    }
     current = (float)((int16_t)((-(~temp + 1)) & 0xFFFF)) * current_lsb;
     if(reversed) current *= -1.0f;
+
+    return FuncRetCode::OK;
+}
+
+FuncRetCode BusSenseINA237::WriteReg(uint8_t reg, uint16_t data) const
+{
+    uint8_t i2c_buffer[3]{reg, (uint8_t)((data >> 8) & 0xFF), (uint8_t)(data & 0xFF)};
+    return i2c->WriteBytes(device_addr, i2c_buffer, 3);
+}
+
+FuncRetCode BusSenseINA237::ReadReg(uint8_t reg, uint16_t* data) const
+{
+    *data = 0x00;
+    uint8_t i2c_buffer[2]{};
+    i2c_buffer[0] = reg;
+    auto ret = i2c->WriteBytes(device_addr, i2c_buffer, 1); // set register pointer
+    if(ret == FuncRetCode::OK)
+    {
+        i2c_buffer[0] = 0x00;
+        ret = i2c->ReadBytes(device_addr, i2c_buffer, 2);
+        if(ret == FuncRetCode::OK)
+            *data = ((uint16_t) i2c_buffer[0] << 8) | i2c_buffer[1];
+    }
+    return ret;
 }
 }
--- src/Sense/bus_sense_ina237.hpp
+++ src/Sense/bus_sense_ina237.hpp
@@ -15,15 +15,49 @@ public:
     BusSenseINA237(I2CBase *_i2c, uint8_t _addr, float _max, float _shunt, bool rev);
     explicit BusSenseINA237(I2CBase *_i2c);
     BusSenseINA237(I2CBase *_i2c, uint8_t _addr, bool rev);
-    FuncRetCode Init() final;
-    void Update() final;
+    FuncRetCode Init() override;
+    FuncRetCode Update() override;
 private:
+    typedef union config // address: 0x00
+    {
+        uint16_t reg{};
+        struct
+        {
+            uint16_t          : 4; /* [3:0] */
+            uint16_t adcrange : 1; /* [4] */
+            uint16_t          : 1; /* [5] */
+            uint16_t convdly  : 8; /* [13:6] */
+            uint16_t          : 1; /* [14] */
+            uint16_t rst      : 1; /* [15] */
+        } bit;
+    } config;
+    typedef union adc_config // address: 0x01
+    {
+        uint16_t reg{};
+        struct
+        {
+            uint16_t avg    : 3; /* [2:0] */
+            uint16_t vtct   : 3; /* [5:3] */
+            uint16_t vshct  : 3; /* [8:6] */
+            uint16_t vbusct : 3; /* [11:9] */
+            uint16_t mode   : 4; /* [15:12] */
+        } bit;
+    } adc_config;
+    typedef union shunt_cal // address: 0x02
+    {
+        uint16_t reg{};
+        struct
+        {
+            uint16_t shunt_cal : 15; /* [14:0] */
+            uint16_t           : 1;  /* [15] */
+        } bit;
+    } shunt_cal;
+    FuncRetCode WriteReg(uint8_t reg, uint16_t data) const;
+    FuncRetCode ReadReg(uint8_t reg, uint16_t* data) const;
     I2CBase *i2c = nullptr;
     uint8_t device_addr = 0x40;
     float max_current = 10.0f;
     float rshunt_ohm = 0.002f;
-    uint8_t i2c_buffer[3] = {0};
-    uint16_t current_cal_reg = 0;
     float current_lsb = 0.001f;
     // float power_lsb = 0.025f;
     bool reversed = false;
