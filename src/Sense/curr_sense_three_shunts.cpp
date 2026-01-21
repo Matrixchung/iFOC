e93076cb253bb3ea560bfd35771a9fd883453764
Matrixchung <xin-zhong@163.com>
2026-01-22 00:31:07 +0800

    Added CurrSenseThreeShunts

---
+++ src/Sense/curr_sense_three_shunts.cpp
@@ -0,0 +1,61 @@
+#include "curr_sense_three_shunts.hpp"
+#include "../DataType/board_config.hpp"
+
+#define config iFOC::BoardConfig().GetConfig()
+
+constexpr uint16_t ZERO_OFFSET_STABLE_TIMES = 1000;
+
+namespace iFOC::Sense
+{
+CurrSenseThreeShunts::CurrSenseThreeShunts(HAL::ADCPortBase* _a, HAL::ADCPortBase* _b, HAL::ADCPortBase* _c,
+    bool rev_a, bool rev_b, bool rev_c) :
+        CurrSenseBase(config.get_current_sense_gain(), config.get_current_sense_shunt_ohm()),
+        JDR_a(_a), JDR_b(_b), JDR_c(_c),
+        Ia_zero_lpf(10), Ib_zero_lpf(10), Ic_zero_lpf(10),
+        Ia_lpf(config.get_current_sense_f_lp()),
+        Ib_lpf(config.get_current_sense_f_lp()),
+        Ic_lpf(config.get_current_sense_f_lp())
+{
+    Ia_zero_lpf.output_prev = Ib_zero_lpf.output_prev = Ic_zero_lpf.output_prev = JDR_a->GetFullRangeVoltage() * 1000.0f * 0.5f;
+    zero_a = zero_b = zero_c = Ia_zero_lpf.output_prev;
+    sign_a = rev_a ? -1 : 1;
+    sign_b = rev_b ? -1 : 1;
+    sign_c = rev_c ? -1 : 1;
+}
+
+void CurrSenseThreeShunts::Update(float Ts)
+{
+    if(zero_offset_calc_times < ZERO_OFFSET_STABLE_TIMES)
+    {
+        shunt_values[0] = 0.0f;
+        shunt_values[1] = 0.0f;
+        shunt_values[2] = 0.0f;
+        return;
+    }
+    shunt_values[0] = Ia_lpf.GetOutput((JDR_a->GetVoltage_mV() - zero_a) * current_factor_mV * (real_t)sign_a, Ts);
+    shunt_values[1] = Ib_lpf.GetOutput((JDR_b->GetVoltage_mV() - zero_b) * current_factor_mV * (real_t)sign_b, Ts);
+    shunt_values[2] = Ic_lpf.GetOutput((JDR_c->GetVoltage_mV() - zero_c) * current_factor_mV * (real_t)sign_c, Ts);
+}
+
+void CurrSenseThreeShunts::UpdateRemainingCurrent(float Ts)
+{
+    const real_t Va = JDR_a->GetVoltage_mV();
+    const real_t Vb = JDR_b->GetVoltage_mV();
+    const real_t Vc = JDR_c->GetVoltage_mV();
+    const real_t Vhalf = JDR_a->GetFullRangeVoltage() * 1000.0f * 0.5f;
+    const real_t Vlow = Vhalf * 0.5f;
+    const real_t Vhigh = Vlow + Vhalf;
+    if(BETWEEN(Va, Vlow, Vhigh) && BETWEEN(Vb, Vlow, Vhigh) && BETWEEN(Vc, Vlow, Vhigh))
+    {
+        if(zero_offset_calc_times < ZERO_OFFSET_STABLE_TIMES) zero_offset_calc_times++;
+        zero_a = Ia_zero_lpf.GetOutput(Va, Ts);
+        zero_b = Ib_zero_lpf.GetOutput(Vb, Ts);
+        zero_c = Ic_zero_lpf.GetOutput(Vc, Ts);
+    }
+}
+
+bool CurrSenseThreeShunts::IsCalibrated()
+{
+    return zero_offset_calc_times >= ZERO_OFFSET_STABLE_TIMES;
+}
+}
---
+++ src/Sense/curr_sense_three_shunts.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "curr_sense_base.hpp"
+#include "../Common/Interface/adc_port_base.hpp"
+#include "../Common/Filter/lowpass_filter.hpp"
+
+namespace iFOC::Sense
+{
+class CurrSenseThreeShunts final : public CurrSenseBase<3>
+{
+    OVERRIDE_NEW();
+    DELETE_COPY_CONSTRUCTOR(CurrSenseThreeShunts);
+public:
+    CurrSenseThreeShunts() = delete;
+    CurrSenseThreeShunts(HAL::ADCPortBase* _a, HAL::ADCPortBase* _b, HAL::ADCPortBase* _c,
+                         bool rev_a, bool rev_b, bool rev_c);
+    void Update(float Ts) override;
+    void UpdateRemainingCurrent(float Ts) override;
+    bool IsCalibrated() override;
+private:
+    HAL::ADCPortBase* JDR_a;
+    HAL::ADCPortBase* JDR_b;
+    HAL::ADCPortBase* JDR_c;
+    real_t zero_a = 0.0f;
+    real_t zero_b = 0.0f;
+    real_t zero_c = 0.0f;
+    Filter::LowpassFilter Ia_zero_lpf;
+    Filter::LowpassFilter Ib_zero_lpf;
+    Filter::LowpassFilter Ic_zero_lpf;
+    Filter::LowpassFilter Ia_lpf;
+    Filter::LowpassFilter Ib_lpf;
+    Filter::LowpassFilter Ic_lpf;
+    uint16_t zero_offset_calc_times = 0;
+    int8_t sign_a = 1;
+    int8_t sign_b = 1;
+    int8_t sign_c = 1;
+};
+}
