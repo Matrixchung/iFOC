0cfdc0ba83564d3a41f630c4751417ec92766d66
Matr1x. <xin-zhong@163.com>
2026-04-11 00:32:30 +0800

    Updated includes for GD32

--- src/HAL/GD32/gd32_include.hpp
+++ src/HAL/GD32/gd32_include.hpp
@@ -5,3 +5,7 @@
 #include "gd32_spi.hpp"
 #include "gd32_uart.hpp"
 #include "gd32_adc_port.hpp"
+#include "gd32_dac_port.hpp"
+#include "gd32_temp_sense_core.hpp"
+#include "gd32_bkp.hpp"
+#include "gd32_can.hpp"
