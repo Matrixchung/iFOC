ede9c6e5db58317c3aa24d57fbb6309ab3cc41c6
Matr1x. <xin-zhong@163.com>
2026-04-13 15:06:47 +0800

    Fixed flash erase procedure of GD32G553

--- src/HAL/GD32/gd32_nvm_storage.cpp
+++ src/HAL/GD32/gd32_nvm_storage.cpp
@@ -6,35 +6,56 @@
 #include "gd32_nvm_address.h"
 
 #if defined(GD32G5X3) // 256 K
+uint32_t get_bank_number(uint32_t address)
+{
+    uint32_t base0_address;
 
-static uint32_t GetPage(const uint32_t addr)
-{
-    return (addr - FLASH_BASE) / FLASH_SECTOR_SIZE_BYTES;
+    if(OB_DUAL_BANK_MODE != (FMC_OBCTL & FMC_OBCTL_DBS)){
+        return FMC_BANK0;
+    }else{
+        base0_address = fmc_bank0_base_address_get();
+        if((address >= base0_address) && (address < (base0_address + MAIN_FLASH_BANK_SIZE * 0x400U))){
+            return FMC_BANK0;
+        }else{
+            return FMC_BANK1;
+        }
+    }
 }
-static uint32_t GetPageSize(const uint32_t page_number)
-{
-    (void)page_number;
-    return fmc_page_size_get();
-}
+// static uint32_t GetPage(const uint32_t addr)
+// {
+//     uint32_t base_address = 0;
+//     const uint32_t bank = get_bank_number(addr);
+//     if(FMC_BANK0 == bank) base_address = fmc_bank0_base_address_get();
+//     else base_address = fmc_bank1_base_address_get();
+//     return (addr - base_address) / fmc_page_size_get();
+// }
+// static uint32_t GetPageSize(const uint32_t page_number)
+// {
+//     (void)page_number;
+//     return fmc_page_size_get();
+// }
 
 namespace iFOC::HAL::NVM
 {
 FuncRetCode Erase(const uint32_t addr, const size_t size)
 {
     if(addr < FLASH_USER_START_ADDR || addr > FLASH_USER_START_ADDR + FLASH_USER_AREA_SIZE) return FuncRetCode::ACCESS_VIOLATION;
-    const auto first_page = GetPage(addr);
-    size_t page_count = 0;
-    size_t size_to_erase = 0;
+    const uint32_t page_size = fmc_page_size_get();
+    uint32_t curr_addr = addr;
+    uint32_t size_to_erase = 0;
+    uint32_t base_address = 0;
+    fmc_unlock();
+    fmc_flag_clear(FMC_FLAG_ENDF | FMC_FLAG_RPERR | FMC_FLAG_OBERR | FMC_FLAG_PGERR | FMC_FLAG_WPERR | FMC_FLAG_OPRERR | FMC_FLAG_PGSERR | FMC_FLAG_PGMERR | FMC_FLAG_PGAERR);
     while(size_to_erase < size)
     {
-        size_to_erase += GetPageSize(first_page + page_count);
-        page_count++;
-    }
-    fmc_unlock();
-    for(uint32_t i = 0; i < page_count; i++)
-    {
-        const uint32_t page_idx = first_page + i;
-        fmc_page_erase(FMC_BANK0, page_idx);
+        const uint32_t bank = get_bank_number(curr_addr);
+        if(FMC_BANK0 == bank) base_address = fmc_bank0_base_address_get();
+        else base_address = fmc_bank1_base_address_get();
+        const uint32_t page_offset = (curr_addr - base_address) / page_size;
+        fmc_page_erase(bank, page_offset);
+        fmc_flag_clear(FMC_FLAG_ENDF | FMC_FLAG_RPERR | FMC_FLAG_OBERR | FMC_FLAG_PGERR | FMC_FLAG_WPERR | FMC_FLAG_OPRERR | FMC_FLAG_PGSERR | FMC_FLAG_PGMERR | FMC_FLAG_PGAERR);
+        size_to_erase += page_size;
+        curr_addr += page_size;
     }
     fmc_lock();
     return FuncRetCode::OK;
