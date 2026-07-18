ae0913fe2e5946539dfc659d556096f0e94419c7
Matr1x. <xin-zhong@163.com>
2026-07-19 03:15:14 +0800

    feat(UARTHS): removed unnecessary MIN() for UARTHS

--- src/Common/Interface/uart_hs_base.cpp
+++ src/Common/Interface/uart_hs_base.cpp
@@ -14,16 +14,18 @@ void UARTHSBase::RemoveIdleCallback()
     idle_cb = nullptr;
 }
 
-uint16_t UARTHSBase::WriteBytes(const uint8_t* data, uint16_t size)
+uint16_t UARTHSBase::WriteBytes(const uint8_t* data, const uint16_t size)
 {
-    const uint16_t len = MIN(size, GetTxAvailable());
+    // const uint16_t len = MIN(size, GetTxAvailable());
+    const uint16_t len = size;
     tx_fifo.put(data, len);
     return len;
 }
 
-uint16_t UARTHSBase::ReadBytes(uint8_t* data, uint16_t size, const bool peek)
+uint16_t UARTHSBase::ReadBytes(uint8_t* data, const uint16_t size, const bool peek)
 {
-    const uint16_t len = MIN(size, GetRxLen());
+    // const uint16_t len = MIN(size, GetRxLen());
+    const uint16_t len = size;
     if(peek) rx_fifo.peek(data, len);
     else rx_fifo.get(data, len);
     return len;
