11c4c220ff8c6e22634900a50efb3fe115bc9a87
Matr1x. <xin-zhong@163.com>
2026-07-20 21:47:17 +0800

    feat(NTC): added arguments for NTC_R25 & NTC_BETA

--- src/Sense/temp_sense_ntc.cpp
+++ src/Sense/temp_sense_ntc.cpp
@@ -1,11 +1,16 @@
 #include "temp_sense_ntc.hpp"
 
-constexpr real_t A = 0.0008736528f;
-constexpr real_t B = 0.000253893f;
-constexpr real_t C = 0.0000001816f;
+constexpr real_t NOMINAL_TEMP_KELVIN = 25.0f + 273.15f; // 298.15K
 
 namespace iFOC::Sense
 {
+TempSenseNTC::TempSenseNTC(HAL::ADCPortBase* _port, real_t _rdiv, real_t ntc_r25, real_t ntc_beta) :
+    port(_port), rdiv(_rdiv), ntc_r25_ohm(ntc_r25), ntc_beta_k(ntc_beta)
+{
+    if(ntc_r25_ohm <= 0.0f) ntc_r25_ohm = 10000.0f;
+    if(ntc_beta_k <= 0.0f) ntc_beta_k = 3950.0f;
+}
+
 TempSenseNTC::TempSenseNTC(HAL::ADCPortBase* _port, real_t _rdiv) : port(_port), rdiv(_rdiv) {}
 
 real_t TempSenseNTC::Update()
@@ -16,9 +21,10 @@ real_t TempSenseNTC::Update()
         temp_celsius = std::numeric_limits<real_t>::max();
         return temp_celsius;
     }
-    const real_t ntc_resistance = rdiv / (((float)port->GetFullRange() / (float)raw_value) - 1.0f);
-    const real_t ntc_Ln = std::logf(ntc_resistance);
-    temp_celsius = (1.0f / (A + B * ntc_Ln + C * ntc_Ln * ntc_Ln * ntc_Ln)) - 273.15f;
+    const real_t ntc_resistance = rdiv * (float)raw_value / (float)(port->GetFullRange() - raw_value);
+    constexpr real_t div_nominal = (1.0f / NOMINAL_TEMP_KELVIN);
+    const real_t inv_temp_kelvin = div_nominal + std::logf(ntc_resistance / ntc_r25_ohm) / ntc_beta_k;
+    temp_celsius = 1.0f / inv_temp_kelvin - 273.15f;
     return temp_celsius;
 }
 }
--- src/Sense/temp_sense_ntc.hpp
+++ src/Sense/temp_sense_ntc.hpp
@@ -7,11 +7,16 @@ namespace iFOC::Sense
 {
 class TempSenseNTC : public TempSenseBase
 {
+    DELETE_COPY_CONSTRUCTOR(TempSenseNTC);
 public:
+    TempSenseNTC(HAL::ADCPortBase* _port, real_t _rdiv,
+        real_t ntc_r25, real_t ntc_beta);
     TempSenseNTC(HAL::ADCPortBase* _port, real_t _rdiv);
     real_t Update() override;
 private:
     HAL::ADCPortBase* port;
     real_t rdiv = 10.0f * 1000.0f;
+    real_t ntc_r25_ohm = 10000.0f; // 10K NTC
+    real_t ntc_beta_k = 3950.0f;
 };
 }
